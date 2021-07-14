ab6f18f119a3df75c0a50226654e15fb5b7ea5dd
Sujay <sujay@abacus.ai>
2021-07-14 22:42:13 +0000

    add multinomial linear dataset

--- synthetic_datasets/__init__.py
+++ synthetic_datasets/__init__.py
@@ -1,3 +1,4 @@
 from .synthetic_gaussian import GaussianLinearRegression, GaussianNonlinearAdditiveRegression, GaussianPiecewiseConstantRegression, GaussianLinearBinary, GaussianNonlinearAdditiveBinary, GaussianPiecewiseConstantBinary
 from .synthetic_mixture import GMLinearRegression, GMNonlinearAdditiveRegression, GMPiecewiseConstantRegression
 from .custom_dataset import CustomDataset
+from .synthetic_multinomial import MultinomialLinearRegression
